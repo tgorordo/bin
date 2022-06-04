887e44cc6cc053125ef0fa077b94678583ad51c4
Thomas (Tom) C. Gorordo <tcgorordo@gmail.com>
2022-06-04 14:19:15 -0700

    init commit

---
+++ bootstrap/dots.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+if ! [ -d "$HOME/.dot" ]; then
+	git clone --seperate-git-dir="$HOME/.dot" https://github.com/tgorordo/dots.git .tmp --recurse-submodules
+	rsync --recursive --verbose --exclude '.git' tmp/ $HOME/ && rm -rf .tmp
+fi
---
+++ bootstrap/github.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if ! [ -x "$(command -v gh)" ]; then
+	if [ -x "$(command -v apt)" ]; then
+		curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+			| sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
+			| sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+		sudo apt update
+		sudo apt install gh
+	else
+		echo "Error: Unsupported distro!" 1>&2 && exit 1
+	fi
+	gh auth login
+fi
