06d27c3f3331688e5dfe57e284c443b15cd7b8d5
Thomas (Tom) C. Gorordo <tcgorordo@gmail.com>
2022-06-12 17:40:04 -0700

    tweaks

--- bootstrap/apt.sh
+++ bootstrap/apt.sh
@@ -10,3 +10,11 @@ fi
 if ! [ -x "$(command -v rsync)" ]; then
 	sudo apt install rsync rclone -y
 fi
+
+if ! [ -x "$(command -v curl)" ]; then
+	sudo apt install curl -y
+fi
+
+if ! [ -x "$(command -v vim)" ]; then
+	sudo apt install neovim -y
+fi
--- bootstrap/dots.sh
+++ bootstrap/dots.sh
@@ -2,4 +2,5 @@
 if ! [ -d "$HOME/.dot" ]; then
 	git clone --separate-git-dir="$HOME/.dot" https://github.com/tgorordo/dots.git .tmp --recurse-submodules
 	rsync --recursive --verbose --exclude '.git' .tmp/ $HOME/ #&& rm -rf .tmp
+	# echo "alias dot='/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME'" >> ~/.bashrc
 fi
